299811360b6a7fee562a1d67e3890ffd35b4e701
yasinyuan <yasinyuan@yasinyuandeMacBook-Air.local>
2024-10-15 00:33:58 +0800

    feat: add react-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import ContextInstance from "./context";
 import { useContext } from "react";
+import { Button } from '@zzxiexin/react-components'
 
 function Name() {
   const params = useContext(ContextInstance);
@@ -12,6 +13,7 @@ export default function App() {
     <div className="App">
       <ContextInstance.Provider value={{ name: "test1`11" }}>
         <Name />
+        <Button type="primary" throttle={3} onClick={() => console.log(1)}>test</Button>
       </ContextInstance.Provider>
     </div>
   );
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const contextInstance = createContext({});
+const contextInstance = createContext({name: ''});
 
 export default contextInstance;
